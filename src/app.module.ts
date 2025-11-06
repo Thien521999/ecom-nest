d9faedcab230269c5bd086f37eefa51693d1a54a
Thien Tran Prep <thientran@prepedu.com>
2025-11-06 10:41:53 +0700

    Tích hợp Zod Validation và Serialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common'
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core'
+import { ZodSerializerInterceptor } from 'nestjs-zod'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AuthModule } from './routes/auth/auth.module'
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter'
+import CustomZodValidationPipe from './shared/pipes/custom-zod-validation.pipe'
 import { SharedModule } from './shared/shared.module'
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: CustomZodValidationPipe,
+    },
+    { provide: APP_INTERCEPTOR, useClass: ZodSerializerInterceptor },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/routes/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { ZodSerializerDto } from 'nestjs-zod'
+import { RegisterBodyDTO, RegisterResDTO } from './auth.dto'
+import { AuthService } from './auth.service'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @ZodSerializerDto(RegisterResDTO)
+  async register(@Body() body: RegisterBodyDTO) {
+    return await this.authService.register(body)
+  }
+
+  @Post('login')
+  async login(@Body() body: any) {
+    return await this.authService.login(body)
+  }
+
+  @Post('refresh-token')
+  @HttpCode(HttpStatus.OK) // để trả về status code 200
+  async RefreshToken(@Body() body: any) {
+    return await this.authService.refreshToken(body.refreshToken)
+  }
+
+  @Post('logout')
+  async logout(@Body() body: any) {
+    return await this.authService.logout(body.refreshToken)
+  }
+}
